b3efef4b08484d527a80bb786cda48833b3786f9
Wind-wind1999 <68496099+Wind-wind1999@users.noreply.github.com>
2020-12-17 15:54:50 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,30 +1,57 @@
-const str = '2';// 2,3,4按键
-// 按键从2开始才有字母
-const a = {
-  2: 'abc', 3: 'def', 4: 'ghi', 
-  5: 'jkl', 6: 'mno',
-  7: 'pqr', 8: 'stu', 9: 'vwxyz'
+let readline = require('readline');
+
+let rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+function ques() {
+	rl.question('请输入[2-9]任意数字或组合：',function(res){
+		if (res == "exit") {
+			rl.close();
+		}
+		combin(res);
+		ques();
+	})
 }
-// 数组->拿到234按键的字母组成的数组->['abc', 'def', 'ghi']
-let code = str.split('').map(ee => a[ee]);
-// 使用递归->获取23数字代表的字母组合的数组后与4代表的字母ghi继续循环组合
-let comb = (arr) => {
-  let temp = []
-  for (let i in arr[0]) {
-    for (let j in arr[1]) {
-      temp.push(`${arr[0][i]}${arr[1][j]}`)
-    }
-  }
-  
-  if (arr.length > 1) {
-	arr.splice(0, 2, temp)
-    comb(arr)
-  } else {
-	  console.log(arr.length)
-    return temp
-  }
-  return arr[0]
+
+ques();
+
+function combin(str) {
+	const a = {
+	  2: 'abc', 3: 'def', 4: 'ghi', 
+	  5: 'jkl', 6: 'mno',
+	  7: 'pqrs', 8: 'tuv', 9: 'wxyz'
+	}
+	
+	//处理输入的字符返回数组
+	let code = str.split('').map(e => a[e]);
+	
+	//处理后的值只有一个就直接输出，不必继续运行
+	if (code.length == 1) {
+		console.log(code[0]);
+		return;
+	}
+	
+	let comb = (arr) => {
+	  let temp = []
+	  for (let i in arr[0]) {
+	    for (let j in arr[1]) {
+	      temp.push(`${arr[0][i]}${arr[1][j]}`)
+	    }
+	  }
+	  
+	  if (arr.length > 1) {
+		arr.splice(0, 2, temp)
+	    comb(arr)
+	  } else {		  
+	    return temp
+	  }
+	  return arr[0]
+	}	 
+	console.log(comb(code))
 }
- 
-console.log(comb(code))
-//adg,adh,adi,aeg,aeh,aei,afg,a
+
+rl.on('close', function(){
+	process.exit(0)
+})
